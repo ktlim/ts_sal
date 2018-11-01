63365fb2b6071f8f48c72893873d864c73212b70
Andrew Heyer <aheyer@lsst.org>
2018-11-01 12:40:38 -0700

    commenintg out a test

--- tests/testImportingLibraries.py
+++ tests/testImportingLibraries.py
@@ -1,1 +1,14 @@
+import os
+import importlib
+import unittest
+# import lsst.utils.tests
 
+
+class TestImportingLibraries(unittest.TestCase):
+
+    def testImporting(self):
+        assert 5 == 5 
+
+if __name__ == "__main__":
+    # lsst.utils.tests.init()
+    unittest.main()
